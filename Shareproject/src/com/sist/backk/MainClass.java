2052730517337cea3558e43c9a95d5495e1d59cf
L-JWon <suzy4ever830@gmail.com>
2022-07-04 18:47:59 +0900

    back-2

--- Shareproject/src/com/sist/backk/MainClass.java
+++ Shareproject/src/com/sist/backk/MainClass.java
@@ -5,7 +5,8 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git-1");
-		System.out.println("Hello Git-2");
+		System.out.println("Hello Git-1");
+		
 	}
 
 }
